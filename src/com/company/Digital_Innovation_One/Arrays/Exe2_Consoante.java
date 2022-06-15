351bf37ea51d0d6bb93bf9c53188c6aca414403e
Rodrigues <diego.executive@gmail.com>
2022-06-15 09:53:45 -0300

    Estudos DIO

---
+++ src/com/company/Digital_Innovation_One/Arrays/Exe2_Consoante.java
@@ -0,0 +1,43 @@
+package com.company.Digital_Innovation_One.Arrays;
+
+import java.util.Scanner;
+
+/*
+Faça um Programa que leia um vetor de 6 caracteres,
+e diga quantas consoantes foram lidas.
+Imprima as consoantes.
+*/
+public class Exe2_Consoante {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String [] consoantes = new String[6];
+        int quantidadeConsoantes = 0;
+
+        int contador = 0;
+        do {
+            System.out.println("Digite uma letra: ");
+            String letra = scan.next();
+
+            if ( !(letra.equalsIgnoreCase("a") |
+                    letra.equalsIgnoreCase("e") |
+                    letra.equalsIgnoreCase("i") |
+                    letra.equalsIgnoreCase("o") |
+                    letra.equalsIgnoreCase("u"))) {
+
+            consoantes[contador] = letra;
+            quantidadeConsoantes++;
+        }
+            contador++;
+
+        } while (contador < consoantes.length);
+
+        System.out.print("Consoantes: ");
+        for ( String consoante : consoantes ) {
+            if (consoante != null)
+            System.out.print(consoante + " ");
+        }
+        System.out.println("\nQuantidade de consoantes: " + quantidadeConsoantes);
+        System.out.println(consoantes.length);
+    }
+}
