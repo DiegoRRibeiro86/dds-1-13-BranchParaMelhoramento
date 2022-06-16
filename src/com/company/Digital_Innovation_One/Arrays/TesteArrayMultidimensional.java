fea2d086e56bdc3e49869984fffbae4242291fa5
Rodrigues <diego.executive@gmail.com>
2022-06-16 16:41:48 -0300

    Estudos DIO

---
+++ src/com/company/Digital_Innovation_One/Arrays/TesteArrayMultidimensional.java
@@ -0,0 +1,9 @@
+package com.company.Digital_Innovation_One.Arrays;
+
+public class TesteArrayMultidimensional {
+    public static void main(String[] args) {
+        int[][] numeros = {{4, 9, 8, 5}, {3, 0, 1, 5}, {1, 2, 7, 4}};
+        System.out.print(numeros[2][2]);
+        }
+    }
+
